3d00f7bdd6cc5be33f3928361d382dbce2c85a9e
Suveksha Chettri <55891372+Suveksha@users.noreply.github.com>
2022-02-15 18:53:01 +0530

    Update BuyStocks.java

--- BuyStocks.java
+++ BuyStocks.java
@@ -1,28 +1,24 @@
-class MaximumDiffrence
-{
-   
-    int maxDiff(int arr[], int arr_size)
-    {
-        int max_diff = arr[1] - arr[0];
-        int min_element = arr[0];
-        int i;
-        for (i = 1; i < arr_size; i++)
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int minSoFar=prices[0];
+        int maxProfit=0;
+        
+        for(int i=0; i<prices.length; i++)
         {
-            if (arr[i] - min_element > max_diff)
-                max_diff = arr[i] - min_element;
-            if (arr[i] < min_element)
-                min_element = arr[i];
+            if(prices[i]<minSoFar)
+                minSoFar=prices[i];
+            
+            int profit=prices[i]-minSoFar;
+            
+            if(maxProfit<profit)
+                maxProfit=profit;
         }
-        return max_diff;
-    }
- 
-   
-    public static void main(String[] args)
-    {
-        MaximumDiffrence maxdif = new MaximumDiffrence();
-        int arr[] = {1, 2, 90, 10, 110};
-        int size = arr.length;
-        System.out.println("MaximumDifference is " +
-                                maxdif.maxDiff(arr, size));
+        
+        if(maxProfit==0)
+            return 0;
+        else
+            return maxProfit;
+        
     }
 }
